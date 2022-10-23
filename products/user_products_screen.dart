2910fe170d111b0a41611e6038e5dbe31c0d9c73
NgoHoangNamB1900243 <113842747+NgoHoangNamB1900243@users.noreply.github.com>
2022-10-23 16:50:22 +0700

    Cai dat chuc nang chon san pham yeu thich

---
+++ products/user_products_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'user_product_list_tile.dart';
+import 'products_manager.dart';
+import '../shared/app_drawer.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+  const UserProductsScreen({super.key});
+
+  Widget build(BuildContext context) {
+    final productsManager = ProductsManager();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: <Widget>[
+          buildAddButton(),
+        ],
+      ),
+      drawer: const AppDrawer(),
+      body: RefreshIndicator(
+        onRefresh: () async => print('refresh products'),
+        child: buildUserProductListView(productsManager),
+      ),
+    );
+  }
+
+  Widget buildUserProductListView(ProductsManager productsManager) {
+    return ListView.builder(
+      itemCount: productsManager.itemCount,
+      itemBuilder: (ctx, i) => Column(
+        children: [
+          UserProductListTile(
+            productsManager.items[i],
+          ),
+          const Divider(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildAddButton() {
+    return IconButton(
+      onPressed: () {
+        print('Go to edit product screen');
+      },
+      icon: const Icon(Icons.add),
+    );
+  }
+}
