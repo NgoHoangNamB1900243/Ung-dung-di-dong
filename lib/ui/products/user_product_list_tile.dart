8e53b877167943855b13fe1f5775baa2adae1187
NgoHoangNamB1900243 <113842747+NgoHoangNamB1900243@users.noreply.github.com>
2022-10-23 20:27:21 +0700

    Xay dung trang hieu chinh san pham

--- lib/ui/products/user_product_list_tile.dart
+++ lib/ui/products/user_product_list_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myshop/ui/products/products_manager.dart';
+import 'package:myshop/ui/screens.dart';
 import 'package:provider/provider.dart';
 import '../../models/product.dart';
 
@@ -51,10 +52,13 @@ class UserProductListTile extends StatelessWidget {
 
   Widget buildEditButton(BuildContext context) {
     return IconButton(
-      onPressed: () {
-        print('Go to edit product screen');
-      },
       icon: const Icon(Icons.edit),
+      onPressed: () {
+        Navigator.of(context).pushNamed(
+          EditProductScreen.routeName,
+          arguments: product.id,
+        );
+      },
       color: Theme.of(context).primaryColor,
     );
   }
